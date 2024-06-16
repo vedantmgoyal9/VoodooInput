3efc097dea715c03c033d415f39a82e92d976277
Vedant <83997633+vedantmgoyal9@users.noreply.github.com>
2024-06-16 11:42:26 +0530

    Use MT 2 ID

--- VoodooInput/VoodooInputIDs.hpp
+++ VoodooInput/VoodooInputIDs.hpp
@@ -11,7 +11,7 @@
 // macOS 10.10-14
 constexpr int kVoodooInputProductMacbook8_1 = 0x272;
 // macOS 15+
-constexpr int kVoodooInputProductMacbookAir10_1 = 0x281;
+constexpr int kVoodooInputProductMacbookAir10_1 = 0x265;
 constexpr int kVoodooInputVendorApple = 0x5ac;
 
 constexpr int kVoodooInputVersionSequoia = 24;
